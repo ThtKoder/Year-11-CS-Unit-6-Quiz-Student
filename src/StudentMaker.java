1a455971c5d14b837410801e2a9df756d2dd47c8
ThtKoder <sanderd2026@student.cis.edu.hk>
2024-02-06 11:22:24 +0800

    done

--- src/StudentMaker.java
+++ src/StudentMaker.java
@@ -1,8 +1,16 @@
 public class StudentMaker {
   
   public static void main(String[] args) {
-    
-    
+    Address myAddress = new Address("Causeway Bay","Tung Lo Wan",180);
+    Student myStudent = new Student(17, "Belinda", "Yu", 12,myAddress);
+    myStudent.setFirstName("Bella");
+    int firstAge = myStudent.getAge();
+    System.out.println(firstAge);
+
+    int age = myStudent.birthday();
+    System.out.println("\n" + age);
+
+    System.out.println("\n" + myStudent);
   }
   
 }
